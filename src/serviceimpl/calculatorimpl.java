9cb7988234edfa792f0959fa3c1f387063c6c59c
akhila194 <akhilapachava194@gmail.com>
2022-08-22 16:11:11 +0530

    first commit

---
+++ src/serviceimpl/calculatorimpl.java
@@ -0,0 +1,59 @@
+package serviceimpl;
+
+
+import java.util.Scanner;
+
+import Interface.Interface1;
+import Interface.Multdiv;
+
+public class calculatorimpl implements Interface1, Multdiv{
+	public static void main(String args[]) {
+		Scanner s1=new Scanner(System.in);
+		System.out.println("Enter number 1");
+		int a=s1.nextInt();
+		System.out.println("Enter number 2");
+		int b=s1.nextInt();
+		
+		Interface1 obj=new calculatorimpl();
+		obj.Addition(a,b);
+		obj.Substraction(a, b);
+		Multdiv obj1=new calculatorimpl();
+		obj1.Multiplication(a, b);
+		obj1.Divison(a, b);
+		
+		
+		
+		
+	}
+	
+
+	@Override
+	public void Addition(int a, int b) {
+		// TODO Auto-generated method stub
+		System.out.println("the addition is:"+ a+b);
+	}
+
+
+	@Override
+	public void Substraction(int c, int d) {
+		// TODO Auto-generated method stub
+		System.out.println("the Substraction is:"+ (c-d));
+		
+		
+	}
+
+
+	@Override
+	public void Multiplication(int num1, int num2) {
+		// TODO Auto-generated method stub
+		System.out.println("the Multiplication is:"+(num1*num2));
+	}
+
+
+	@Override
+	public void Divison(int num3, int num4) {
+		// TODO Auto-generated method stub
+		System.out.println("the divison is:"+(num3/num4));
+	}
+
+}
